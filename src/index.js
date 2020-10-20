5b55810830fce496a9d0adcb9744272b83d1dc03
Wlad <yanushevskyv@gmail.com>
2020-10-20 12:41:06 +0300

    final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if(n>=0)
+    {
+    str = String(n)  
+    return Number(str.split("").reverse().join(""))
+    }
+    else {
+    str = String(n*(-1))
+    return Number(str.split("").reverse().join(""))
+    }
 }
